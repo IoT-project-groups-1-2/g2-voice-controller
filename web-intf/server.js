b06e3c9759fa6a3387d13dffa17a0ed5d05c1bdb
longph <longphamduy2002@gmail.com>
2022-11-21 15:18:37 +0200

    add speech recognition feature (beta)

--- web-intf/server.js
+++ web-intf/server.js
@@ -6,12 +6,21 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 //Defining constants
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    res.send("AYYO WHATZ GUD!");
+    res.render('home');
 });
 
 
 app.listen(PORT);
+
+
+
+
+
+

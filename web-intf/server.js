ebb3ba834ca702770dc53d9c157ef10ae70321c8
Kendrick807 <kendrick.kwong@metropolia.fi>
2022-11-28 14:49:09 +0200

    Revert "Abort works perfectly and MQTT added."
    This reverts commit 90e226616e487edca9eb7e0123431229243d67bf.

--- web-intf/server.js
+++ web-intf/server.js
@@ -3,50 +3,21 @@
 //Getting node modules
 const express = require('express');
 const path = require('path');
-const mqtt = require('mqtt');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-
-
-
-
 //Defining constants
-// const port = process.env.PORT || 3000;
-const broker_url = 'mqtt://127.0.0.1:1883';
-const client = mqtt.connect(broker_url, { clientId: 'node', clean: true });
-const settings_topic = "controller/settings";
-const status_topic = "controller/status";
-
-client.subscribe(status_topic, (err) => {
-    console.log("status topic subscribed.")
-        if (!err) {
-                client.publish(status_topic, 'Hello mqtt');
-
-        }
-});
+const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     res.render('home');
 });
 
 
-app.get('/voice-data',(req, res) => {
-
-    client.on('message', (topic, msg) => {
-        client.removeAllListeners();
-        msg = msg.toString();
-        console.log(msg);
-        res.json(msg);
-    });
-
-});
-
-
-app.listen(3000);
+app.listen(PORT);
 
 
 

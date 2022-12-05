f9ce7f5e1ec234a7edf683593df7fd60368d297a
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-12-05 21:39:03 +0200

    rename previous i2s output program and add program to test wav file playback

--- src/i2s_out_tone.py
+++ src/i2s_out_tone.py
@@ -3,13 +3,15 @@ import struct
 from machine import I2S
 from machine import Pin
 
+twopi = 6.283185
+
 
 def make_tone(rate, bits, frequency):
     # create a buffer containing the pure tone samples
     samples_per_cycle = rate // frequency
     sample_size_in_bytes = bits // 8
     samples = bytearray(samples_per_cycle * sample_size_in_bytes)
-    volume_reduction_factor = 32
+    volume_reduction_factor = 16
     range = pow(2, bits) // 2 // volume_reduction_factor
 
     if bits == 16:
@@ -18,25 +20,26 @@ def make_tone(rate, bits, frequency):
         format = "<l"
 
     for i in range(samples_per_cycle):
-        sample = range + int((range - 1) * math.sin(2 * math.pi * i / samples_per_cycle))
+        
+        #y(t) = A * sin(2 * pi * f * t)
+        sample = int((range) * math.sin(twopi * i / samples_per_cycle))
         struct.pack_into(format, samples, i * sample_size_in_bytes, sample)
 
     return samples
 
-    # ======= I2S CONFIGURATION =======
-
+# ======= I2S CONFIGURATION =======
 
 SCK_PIN = 16
 WS_PIN = 17
 SD_PIN = 18
 I2S_ID = 0
-BUFFER_LENGTH_IN_BYTES = 2000
+BUFFER_LENGTH_IN_BYTES = 1000
 
 # ======= AUDIO CONFIGURATION =======
-TONE_FREQUENCY_IN_HZ = 2000
+TONE_FREQUENCY_IN_HZ = 440
 SAMPLE_SIZE_IN_BITS = 16
 FORMAT = I2S.MONO  # only MONO supported in this example
-SAMPLE_RATE_IN_HZ = 44140
+SAMPLE_RATE_IN_HZ = 12000
 # ======= AUDIO CONFIGURATION =======
 
 audio_out = I2S(

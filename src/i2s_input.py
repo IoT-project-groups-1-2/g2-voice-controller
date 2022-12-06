5b16cf81a855fa70a8791ba976d93105163c6d34
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-12-06 20:53:27 +0200

    rm unneeded configurations

--- src/i2s_input.py
+++ src/i2s_input.py
@@ -19,89 +19,34 @@ import uasyncio as asyncio
 from machine import I2S
 from machine import Pin
 
-if os.uname().machine.count("PYBv1"):
+from sdcard import SDCard
+from machine import SPI
 
-    # ======= I2S CONFIGURATION =======
-    SCK_PIN = "Y6"
-    WS_PIN = "Y5"
-    SD_PIN = "Y8"
-    I2S_ID = 2
-    BUFFER_LENGTH_IN_BYTES = 40000
-    # ======= I2S CONFIGURATION =======
-
-elif os.uname().machine.count("PYBD"):
-    import pyb
-
-    pyb.Pin("EN_3V3").on()  # provide 3.3V on 3V3 output pin
-    os.mount(pyb.SDCard(), "/sd")
-
-    # ======= I2S CONFIGURATION =======
-    SCK_PIN = "Y6"
-    WS_PIN = "Y5"
-    SD_PIN = "Y8"
-    I2S_ID = 2
-    BUFFER_LENGTH_IN_BYTES = 40000
-    # ======= I2S CONFIGURATION =======
-
-elif os.uname().machine.count("ESP32"):
-    from machine import SDCard
-
-    sd = SDCard(slot=2)  # sck=18, mosi=23, miso=19, cs=5
-    os.mount(sd, "/sd")
-
-    # ======= I2S CONFIGURATION =======
-    SCK_PIN = 32
-    WS_PIN = 25
-    SD_PIN = 33
-    I2S_ID = 0
-    BUFFER_LENGTH_IN_BYTES = 40000
-    # ======= I2S CONFIGURATION =======
-
-elif os.uname().machine.count("Raspberry"):
-    from sdcard import SDCard
-    from machine import SPI
-
-    cs = Pin(13, machine.Pin.OUT)
-    spi = SPI(
-        1,
-        baudrate=1_000_000,  # this has no effect on spi bus speed to SD Card
-        polarity=0,
-        phase=0,
-        bits=8,
-        firstbit=machine.SPI.MSB,
-        sck=Pin(14),
-        mosi=Pin(15),
-        miso=Pin(12),
+cs = Pin(13, machine.Pin.OUT)
+spi = SPI(
+    1,
+    baudrate=1_000_000,  # this has no effect on spi bus speed to SD Card
+    polarity=0,
+    phase=0,
+    bits=8,
+    firstbit=machine.SPI.MSB,
+    sck=Pin(14),
+    mosi=Pin(15),
+    miso=Pin(12),
     )
 
-    sd = SDCard(spi, cs)
-    sd.init_spi(25_000_000)  # increase SPI bus speed to SD card
-    os.mount(sd, "/sd")
+sd = SDCard(spi, cs)
+sd.init_spi(25_000_000)  # increase SPI bus speed to SD card
+os.mount(sd, "/sd")
 
-    # ======= I2S CONFIGURATION =======
-    SCK_PIN = 16
-    WS_PIN = 17
-    SD_PIN = 18
-    I2S_ID = 0
-    BUFFER_LENGTH_IN_BYTES = 60000  # larger buffer to accommodate slow SD card driver
-    # ======= I2S CONFIGURATION =======
+# ======= I2S CONFIGURATION =======
+SCK_PIN = 16
+WS_PIN = 17
+SD_PIN = 18
+I2S_ID = 0
+BUFFER_LENGTH_IN_BYTES = 60000  # larger buffer to accommodate slow SD card driver
+# ======= I2S CONFIGURATION =======
 
-elif os.uname().machine.count("MIMXRT"):
-    from machine import SDCard
-
-    sd = SDCard(1)  # Teensy 4.1: sck=45, mosi=43, miso=42, cs=44
-    os.mount(sd, "/sd")
-
-    # ======= I2S CONFIGURATION =======
-    SCK_PIN = 21
-    WS_PIN = 20
-    SD_PIN = 8
-    I2S_ID = 1
-    BUFFER_LENGTH_IN_BYTES = 40000
-    # ======= I2S CONFIGURATION =======
-
-else:
-    print("Warning: program not tested with this board")
 
 # ======= AUDIO CONFIGURATION =======
 WAV_FILE = "mic.wav"

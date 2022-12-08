c3dc74a1246cb81242a6d83cc95f97de7b291d02
longph <longphamduy2002@gmail.com>
2022-12-08 09:21:10 +0200

    css mod

--- web-intf/server.js
+++ web-intf/server.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 //Defining constants
 const PORT = process.env.PORT || 3000;
 const broker_url = 'mqtt://127.0.0.1:1883';
-const mongo_url = 'mongodb://localhost:27017';
+const mongo_url = '';
 const client = mqtt.connect(broker_url, { clientId: 'node', clean: true });
 const settings_topic = "controller/settings";
 const status_topic = "controller/status";

6c90711922246ee7626c8687571effd0d9b33427
longph <longphamduy2002@gmail.com>
2022-12-11 00:06:17 +0200

    mqtt working temporarily

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import network
 import urequests
 import random
 import credentials as creds
-
+from umqtt.simple import MQTTClient
 
 """
 Pins configs
@@ -36,6 +36,7 @@ i2c = I2C(0, sda = sdaPIN, scl = sclPIN, freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
 lcd.putstr("Initializing...")
 
+
 """
 Network config
 """
@@ -47,6 +48,24 @@ print(wlan.isconnected())
 def fetch_playlist():
     res = urequests.get("https://api.npoint.io/7a1f386d7c2ca53aded3").json()
     return res
+
+
+mqtt_server = 'broker.hivemq.com'
+client_id = 'PicoW'
+user_t = creds.mqtt_usr
+password_t = creds.mqtt_password
+topic_pub = 'test'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, user=user_t, password=password_t, keepalive=60)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    
+    return client
 
 
 
@@ -64,10 +83,12 @@ def play_tone(freq, msec):
 def loop():
     playlist = fetch_playlist()
     nr_of_songs = len(playlist)
+    client = mqtt_connect()
     while True: 
         rand_index = random.choice(range(0, nr_of_songs - 1))
         lcd.clear()
         lcd.putstr(playlist[rand_index]["Name"])
+        client.publish(topic_pub, "Playing" + playlist[rand_index]["Name"])
         tune = RTTTL(playlist[rand_index]["rtttl"])
         led.value(1)
         for freq, msec in tune.notes():
